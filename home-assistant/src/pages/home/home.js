a4d022eefaf15b992922bf6718bc25e19cbceec4
quvi007 <1805007@ugrad.cse.buet.ac.bd>
2022-08-04 19:57:18 +0600

    updated frontend

--- home-assistant/src/pages/home/home.js
+++ home-assistant/src/pages/home/home.js
@@ -1,8 +1,8 @@
 import { Box } from "@chakra-ui/react";
-import Login from "../../pages/login";
-import AudioRecorder from "../recorder2";
-import Recorder2 from '../components/recorder2';
-import MediaRecorderM from '../components/media-recorder';
+import Login from "../login";
+import AudioRecorder from "../../components/recorder2";
+import Recorder2 from '../../components/recorder2';
+import MediaRecorderM from '../../components/media-recorder';
 
 function Home() {
     return (
