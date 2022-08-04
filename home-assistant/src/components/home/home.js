fc162386ca7ef183395a17de61999ef2e0e12de4
Sohaib03 <sohaibbinmusa@gmail.com>
2022-08-04 19:26:44 +0600

    React Router Installed

---
+++ home-assistant/src/components/home/home.js
@@ -0,0 +1,25 @@
+import { Box } from "@chakra-ui/react";
+import Login from "../../pages/login";
+import AudioRecorder from "../recorder2";
+import Recorder2 from '../components/recorder2';
+import MediaRecorderM from '../components/media-recorder';
+
+function Home() {
+    return (
+        <div className="App">
+            <Login></Login>
+            <Box>
+                <AudioRecorder />
+            </Box>
+            <Box>
+                <MediaRecorderM />
+            </Box>
+            <Box>
+                <Recorder2></Recorder2>
+            </Box>
+        </div>
+
+    )
+}
+
+export default Home;
